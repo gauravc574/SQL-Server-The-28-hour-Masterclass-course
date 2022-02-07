a6e86e9c0326079bb37cf774e25fa48235516e19
gauravc574 <gauravchawre580@gmail.com>
2022-02-08 00:04:11 +0530

    Day 2 started

--- Day1.sql
+++ Day1.sql
@@ -22,3 +22,28 @@ DROP DATABASE Exercise2;
 
 
 
+--Creating table
+
+--Syntax : CREATE TABLE <Table Name> (<Column name> <Type of column>)
+USE [PRACTICE];
+
+
+CREATE TABLE Employee(EmpNo int);
+sp_rename 'Employee','Employee2'
+sp_rename 'Employee2','Employee'
+
+
+DROP TABLE Employee;
+
+
+CREATE TABLE Simple_Table(column_1 nvarchar(10), column_2 nvarchar(10));
+
+SP_RENAME 'simple_table','Simple_table1'
+
+DROP TABLE Simple_Table1
+
+
+--insert data into database 
+
+
+--Syntax :- insert into <Table name> values (<values>,<values>)
