70d2401b9cc8e814d17f851528c1512ab08cb533
gauravc574 <gauravchawre580@gmail.com>
2022-02-08 23:15:17 +0530

    Day 2 done

--- Day1.sql
+++ Day1.sql
@@ -47,3 +47,61 @@ DROP TABLE Simple_Table1
 
 
 --Syntax :- insert into <Table name> values (<values>,<values>)
+--INTO is not mandatory
+
+USE [PRACTICE];
+
+CREATE TABLE Student( StudentId int, StudentName varchar(25));
+
+
+INSERT  Student values (1,'Gaurav');
+INSERT INTO Student values 
+(2,'Suraj'),
+(3,'Himanshu'),
+(4,'Kiran');
+
+  
+SELECT StudentName, StudentId
+FROM Student
+WHERE StudentName='GAURAV';
+
+
+--Update data
+
+-- Syntax : UPDATE <Table name> SET <Column Name> = <Updated value> Where <Condition>
+
+SELECT * from Student;
+UPDATE Student SET StudentName= 'SHANTANU' WHERE StudentId=1;
+
+DROP TABLE Student
+
+--We can use [] to use column name with space
+--If we want to use terms reserved by SQL we can use [] for them as well
+
+
+--DELETE data from table
+
+--Syntax - DELETE FROM <Table name> WHERE <condition>
+--If no condition is provided with delete then it will behave as truncate 
+
+DELETE FROM Student WHERE STUDENTID=4;
+
+
+--Updating 2 columns
+
+UPDATE Student SET StudentName='Priya',StudentId=4 WHERE StudentId=3;
+
+
+--Excercise 
+USE [PRACTICE]
+
+CREATE TABLE StudentInfo([Name] varchar(200), FirstName char(10), Age int);
+
+INSERT INTO StudentInfo VALUES
+('Gaurav Chawre', 'Gaurav', 22),
+('Priyanka Khanna','Priyanka',23),
+('Shantanu Hirwatkar','Shantanu',25)
+
+
+Select * from StudentInfo where FirstName='Gaurav';
+UPDATE StudentInfo set Age = 24 wHERE FirstName='Gaurav'
