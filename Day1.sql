e6371cb6efc38af867557cdbc2aa2a078bb2a22a
gauravc574 <gauravchawre580@gmail.com>
2022-02-10 00:25:52 +0530

    Day 3 comleted

--- Day1.sql
+++ Day1.sql
@@ -105,3 +105,66 @@ INSERT INTO StudentInfo VALUES
 
 Select * from StudentInfo where FirstName='Gaurav';
 UPDATE StudentInfo set Age = 24 wHERE FirstName='Gaurav'
+
+
+USE [PRACTICE];
+GO
+--Aliases
+
+/*******************************************************************************
+********************** The aliases on the columns    ***************************
+********************************************************************************/
+USE FORMATION
+GO
+
+--Deepening in the Transact-SQL will be done on the "Contact" table. 
+--
+
+--We create a table named "Contact" 
+
+
+create table Contact ([Name] varchar (200),firstname varchar (200), 
+age int, gender char (1), date_of_birth date)
+
+GO
+
+insert into contact values 
+
+('Jackson','Mickael',18,'F','1976-08-04'),
+('Taylor','Liz',16,'F','1978-08-02'),
+('Grace','Kelly',17,'F','1987-05-02'),
+('Eastwood','Clint',23,'M','1970-05-06'),
+('Willis','Bruce',22,'M','1983-08-02'),
+('Dicaprio','Leonardo',22,'M','1983-08-02'),-- same age as Bruce Willis
+('Pitt','Brad',37,'M','1975-08-02'),
+('Clooney','George',22,'M','1983-01-01'),
+('Monroe','Marylin',48,'M','1960-06-12'),
+('Roberts','Julia',47,'F','1968-06-12'),
+('Jolie','Angelina',28,'M','1972-07-24'),
+('Thuillier','olivier',41,'M','1976-08-12'),
+('Chuillier','olivier',41,'M','1976-08-12'),
+('Stone','Sharon',34,'F','1983-05-08'),
+('Willis','Bruce',22,'M','1983-08-02')-- twice the same value in the table, it's a duplicate
+
+-- SELECT ?
+
+SELECT * FROM Contact 
+
+-- If I want to rename the name of my column Name by Nickname? 
+
+select [name] as Nickname from contact
+
+-- What if I want to rename the names of all the columns? 
+
+select [name] as Nickname, firstname as Superman, age as Spiderman,gender as Batman, 
+date_of_birth as Hulk from contact
+
+-- You can see that all the columns have been renamed :)
+
+Select [name ] as [First Name] from Contact  where [Name] like '%o'; 
+
+SELECT * FROM Contact
+GO
+
+SELECT * FROM Contact
+WHERE AGE LIKE '2%';
